afec31e54824b5a828b3f195fb0d37544bb57b8c
qinlinchong <qinlinchong@hd123.com>
2026-08-10 14:45:20 +0800

    feat: 添加滚动条全局样式

--- src/main.ts
+++ src/main.ts
@@ -3,5 +3,6 @@ import { createPinia } from "pinia";
 import App from "./App.vue";
 import router from "./router";
 import "virtual:uno.css";
+import "./styles/global.css";
 
 createApp(App).use(createPinia()).use(router).mount("#app");
